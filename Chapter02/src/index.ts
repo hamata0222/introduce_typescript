420a63a28ceca2c878031aa462bfd28d88fe5cfe
hamata0222 <jp.ne.hiroshi@gmail.com>
2022-09-04 10:27:02 +0900

    Test TypeScript type checker.

--- Chapter02/src/index.ts
+++ Chapter02/src/index.ts
@@ -1,1 +1,12 @@
 console.log("Hello TypeScript!");
+
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple + 4;
+
+let fushigi = 3 + 1;
+console.log(`${fushigi}`);
